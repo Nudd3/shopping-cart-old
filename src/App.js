f83c09ecfe1ae853656d33552cd99bd4b22d10ec
Noak Pettersson <noake12@gmail.com>
2023-03-06 01:22:38 +0100

    Links in navbar now redirects to corresponding pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
+import './App.scss';
 
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Shop } from './pages/Shop';
+import { About } from './pages/About';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar />
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
