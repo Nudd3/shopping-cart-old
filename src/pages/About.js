e7eff9473d4a9c2bf3311297a63a09c3bf000a09
Noak Pettersson <noake12@gmail.com>
2023-03-18 19:12:07 +0100

    Added same background to About as in Home. Also fixed the styling and responsiveness of the About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,8 @@ export const About = () => {
   return (
     <div className='about'>
       <div className='about-info'>
-        <div>
-          <h1>About</h1>
+        <div className='about-about'>
+          <h2>About</h2>
           <p>
             Note that this is merely a portfolio project and by no means is this
             shop working, nor is it ever going to. This about section is just
@@ -11,7 +11,7 @@ export const About = () => {
             me write this particular informatory section.
           </p>
         </div>
-        <div>
+        <div className='about-images'>
           <h2>Images</h2>
           <p>
             All images used for this project is taken from unsplash.com. For
